fb66b0dc0ff99ed976e280fd346007f66617b254
APPo1102 <95098057+APPo1102@users.noreply.github.com>
2023-10-14 23:49:25 +0800

    Base file upload
    Create base file

---
+++ wordpress-line-notify-business.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: WordPress LINE Notify Business
+ * Description: This plugin can send message by LINE Business
+ * Version:     0.0.1
+ * Author:      APPo1102
+ * Author URI:  https://github.com/APPo1102/wordpress-line-notify-business
+ * Text Domain: wordpress-line-notify-business
+ * Domain Path: /languages
+ */
+
+
+new wordpress_line_notify_business();
+
+class wordpress_line_notify_business{
+
+   
+}
